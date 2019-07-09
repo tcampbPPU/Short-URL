85b27f1b5e62c9f703bef3ab34bdb55324f201d8
Tanner Campbell <tannercampbell@Tanners-MBP-New.fios-router.home>
2019-07-09 19:47:54 -0400

    adding api backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(express.json({ extended : false }));
+
+// Declare Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
