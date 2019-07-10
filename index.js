132cd5e5268c0eb20b5938327d9df126589f898f
Tanner Campbell <tannercampbell@Tanners-MBP-New.fios-router.home>
2019-07-09 21:50:36 -0400

    basic frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 // connect to database
@@ -7,10 +8,16 @@ connectDB();
 
 app.use(express.json({ extended : false }));
 
+app.use(express.static(path.join(__dirname, 'views')));
+
 // Declare Routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
 const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+  });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
